8f224e074b2d1babfab9bfd9dea6290d639c40f5
PressJump <40802367+PressJump@users.noreply.github.com>
2023-02-05 19:26:24 +1100

    Front Page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,9 @@ import Image from "next/image";
 import reactLogo from "../assets/react.svg";
 import tauriLogo from "../assets/tauri.svg";
 import nextLogo from "../assets/next.svg";
+import honeycomb from "../assets/honeycomb.png";
+import bee from "../assets/bee.png";
+import {ArrowPathRoundedSquareIcon, AcademicCapIcon} from '@heroicons/react/24/solid'
 
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
@@ -15,61 +18,39 @@ function App() {
   }
 
   return (
-    <div className="container">
-      <h1>Welcome to Tauri!</h1>
-
-      <div className="row">
-        <span className="logos">
-          <a href="https://nextjs.org" target="_blank">
-            <Image
-              width={144}
-              height={144}
-              src={nextLogo}
-              className="logo next"
-              alt="Next logo"
-            />
-          </a>
-        </span>
-        <span className="logos">
-          <a href="https://tauri.app" target="_blank">
-            <Image
-              width={144}
-              height={144}
-              src={tauriLogo}
-              className="logo tauri"
-              alt="Tauri logo"
-            />
-          </a>
-        </span>
-        <span className="logos">
-          <a href="https://reactjs.org" target="_blank">
-            <Image
-              width={144}
-              height={144}
-              src={reactLogo}
-              className="logo react"
-              alt="React logo"
-            />
-          </a>
-        </span>
+    
+    <div className="flex h-screen justify-center items-center flex-col">
+      <div className="inline-flex items-center mb-10">
+        <Image src={honeycomb} height={60} width={60} alt={"honeycomb"} className="mr-2"></Image>
+        <Image src={bee} alt={"bee"} className="ml-2 bee"></Image>
+        <div className="text-4xl font-bold text-gray-900 ml-2">CombConvert</div>
       </div>
-
-      <p>Click on the Tauri, Next, and React logos to learn more.</p>
-
-      <div className="row">
-        <div>
-          <input
-            id="greet-input"
-            onChange={(e) => setName(e.currentTarget.value)}
-            placeholder="Enter a name..."
-          />
-          <button type="button" onClick={() => greet()}>
-            Greet
+      <div className="flex flex-col">
+          <button className="p-3 rounded-lg max-w-xs inline-flex items-center hover:bg-gray-50">
+            <div className="p-2 shadow-md rounded-lg mr-5">
+              <ArrowPathRoundedSquareIcon className="w-8"/>
+            </div>
+            <div className="text-left">
+              <div className="text-xl font-bold text-gray-900">Convert a Video</div>
+              <div className="text-gray-500">Convert your videos </div>
+            </div>
+          </button>
+          <button className="p-3 rounded-lg max-w-xs inline-flex items-center hover:bg-gray-50">
+            <div className="p-2 shadow-md rounded-lg mr-5">
+              <AcademicCapIcon className="w-8"/>
+            </div>
+            <div className="text-left">
+              <div className="text-xl font-bold text-gray-900">About Us</div>
+              <div className="text-gray-500">About CombConvert </div>
+            </div>
           </button>
         </div>
-      </div>
+        {/* Or drag and drop here text */}
+        <p className="font-light text-gray-500">
+          Or drag and drop video here
+        </p>
 
-      <p>{greetMsg}</p>
+
     </div>
   );
 }
